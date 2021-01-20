3862e8e03f5194b3371118cfc85bc9b6b9b57f8e
De0 <deobfuscated1@gmail.com>
2021-01-19 20:59:34 -0600

    Separate plugin tests

--- src/test/java/de0/CoxPluginsTest.java
+++ src/test/java/de0/CoxPluginsTest.java
@@ -3,15 +3,15 @@ package de0;
 import de0.coxthieving.CoxThievingPlugin;
 import de0.coxtimers.CoxTimersPlugin;
 import de0.coxvanguards.CoxVanguardsPlugin;
-import de0.loadinglines.LoadingLinesPlugin;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class PluginHubPluginsTest {
+public class CoxPluginsTest {
 
   @SuppressWarnings("unchecked")
   public static void main(String[] args) throws Exception {
-    ExternalPluginManager.loadBuiltin(CoxTimersPlugin.class, CoxThievingPlugin.class, LoadingLinesPlugin.class, CoxVanguardsPlugin.class);
+    ExternalPluginManager.loadBuiltin(CoxTimersPlugin.class,
+        CoxVanguardsPlugin.class, CoxThievingPlugin.class);
     RuneLite.main(args);
   }
 
