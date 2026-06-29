fe5d9022822e59cc81723273fba2444f56282e6d
umer-rs <72721490+umer-rs@users.noreply.github.com>
2026-06-29 16:51:53 +0100

    1.1.1: remove default=off, update build & authors

--- src/main/java/de0/loadinglines/LoadingLinesPlugin.java
+++ src/main/java/de0/loadinglines/LoadingLinesPlugin.java
@@ -9,7 +9,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 
-@PluginDescriptor(name = "Loading Lines", description = "Draw loading lines on screen", enabledByDefault = false)
+@PluginDescriptor(name = "Loading Lines", description = "Draw loading lines on screen")
 public class LoadingLinesPlugin extends Plugin {
 
   @Inject
